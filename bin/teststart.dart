1a951052cf9732a83098a6a0cb0e63da28a136e2
veelected <vaquiino@gmail.com>
2022-10-21 12:16:08 -0400

    Initial commit

---
+++ bin/teststart.dart
@@ -0,0 +1,5 @@
+import 'package:teststart/teststart.dart' as teststart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${teststart.calculate()}!');
+}
---
+++ test/teststart_test.dart
@@ -0,0 +1,8 @@
+import 'package:teststart/teststart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
